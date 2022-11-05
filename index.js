c141e590b6e7d8e55bfa30b25d824cd22ce60fa2
christianBassey2002 <Downtownsaint750@gmail.com>
2022-11-05 16:25:00 -0700

    i coded a background changer with js

---
+++ index.js
@@ -0,0 +1,10 @@
+// let colors = ["black","blue","red","pink","green","grey","khaki","teal"];
+
+// let button = document.getElementById('button');
+
+// button.addEventListener('click',function(){
+//      var randomcolor = colors[Math.floor(Math.random() *colors.length)]
+//     var container = document.getElementById('container');
+//     container.style.background = randomcolor;
+
+// })
